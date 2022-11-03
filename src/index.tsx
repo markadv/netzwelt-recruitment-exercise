ec013619158576aaf6e708cbf58cfb1bc5f9adbb
Mark Timothy Advento <mark@adventodesign.com>
2022-11-03 13:53:46 +0800

    Adding base routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import App from "./App";
+
+/* Routes, Pages */
+import ErrorPage from "./ErrorPage";
+import Home from "./routes/Home";
+import Login from "./routes/Login";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: "/account/login",
+		element: <Login />,
+		errorElement: <ErrorPage />,
+	},
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
